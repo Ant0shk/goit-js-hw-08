50b8a85978cb98f46566f24b996fa097cd965c88
AntoninaYatsenko <dp130688jaj@gmail.com>
2023-08-18 16:19:24 +0200

    push new

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,25 @@
 import throttle from 'lodash.throttle';
+
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
 const formElements = document.querySelector('.feedback-form');
-console.dir(formElements);
-console.dir(formElements.elements.email);
-console.dir(formElements.elements.message);
 const KEY_LS = 'feedback-form-state';
-
-const storageObj = JSON.parse(localStorage.getItem(KEY_LS)) || {};
-console.log(storageObj);
-
-console.log(Boolean(storageObj));
-
-
-function populetedText() {
-  for (const key in storageObj) {
-    console.log(key);
-    console.log(storageObj[key]);
-    console.log(formElements.elements[key].value);
-    formElements.elements[key].value = storageObj[key];
-    console.log(formElements.elements[key].value);
-  }
+formElements.addEventListener('input', throttle(handlerInput, 500));
+formElements.addEventListener('submit', handlerSubmit);
+let data = JSON.parse(localStorage.getItem(KEY_LS)) ?? {};
+const { email, message } = formElements.elements;
+email.value = data.email ?? '';
+message.value = data.message ?? '';
+function handlerInput(event) {
+  data[event.target.name] = event.target.value;
+  localStorage.setItem(KEY_LS, JSON.stringify(data));
 }
-populetedText();
-
-formElements.addEventListener('input', throttle(callbackStorageHandler, 500));
-function callbackStorageHandler(event) {
-  storageObj[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(storageObj));
-  console.log(event.target.name, storageObj[event.target.name]);
-  console.log(localStorage.getItem(KEY_LS));
-}
-
-formElements.addEventListener('submit', event => {
+function handlerSubmit(event) {
   event.preventDefault();
-  console.log('сабмит', storageObj);
+  console.log(data);
+  formElements.reset();
   localStorage.removeItem(KEY_LS);
-  event.currentTarget.reset();
-});
+  data = {};
+}
